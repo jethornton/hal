aad1ed96d196ac3909e8282ae122e74cc6db16c6
John Thornton <dev@gnipsel.com>
2024-02-13 07:36:04 -0600

    add working example
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- startup.py
+++ startup.py
@@ -3,6 +3,16 @@ from PyQt6.QtWidgets import QPushButton
 
 import hal
 
+'''
+This works!
+self.halcomp = hal.component('mytest')
+self.out2 = self.halcomp.newpin('out2', hal.HAL_BIT, hal.HAL_OUT)
+self.test_hal = QPushButton('Test HAL')
+self.test_hal.setCheckable(True)
+self.test_hal.toggled.connect(lambda: self.out2.set(self.test_hal.isChecked()))
+
+'''
+
 def setup_hal(parent):
 	for button in parent.findChildren(QPushButton):
 		if button.property('function') == 'hal_pin':
@@ -11,22 +21,22 @@ def setup_hal(parent):
 			for prop in props:
 				prop = str(prop, 'utf-8')
 				if prop.startswith('pin_'):
-					#print(f'prop {prop}')
+					print(f'prop {prop}')
 					pin_settings = button.property(prop).split(',')
 					name = button.objectName()
-					#print(f'name {name}')
+					print(f'name {name}')
 					pin_name = pin_settings[0]
-					#print(f'pin_name {pin_name}')
+					print(f'pin_name {pin_name}')
 					pin_type = getattr(hal, f'{pin_settings[1].upper().strip()}')
-					#print(f'pin_type {pin_type}')
+					print(f'pin_type {pin_type}')
 					pin_dir = getattr(hal, f'{pin_settings[2].upper().strip()}')
-					#print(f'pin_dir {pin_dir}')
+					print(f'pin_dir {pin_dir}')
 					parent.hal_comp = hal.component('jet')
-					#print(f'{parent.hal_comp.getprefix()}')
+					print(f'{parent.hal_comp.getprefix()}')
 					setattr(parent, f'{prop}', parent.hal_comp.newpin(pin_name, pin_type, pin_dir))
-					#print(getattr(parent, f'{prop}').name)
-					#print(parent.hal_comp[pin_name])
-					#print(parent.hal_comp.getpins())
+					print(getattr(parent, f'{prop}').name)
+					print(parent.hal_comp[pin_name])
+					print(parent.hal_comp.getpins())
 					#print(dir(parent.hal_comp))
 					#print(f'parent.{prop}')
 					#pin = parent.hal_comp.newpin(pin_name, pin_type, pin_dir)
